68942f3e6cd6da73028fc43a057dc98ba15808e2
kyhslam <kyhslam@gmail.com>
2021-09-05 17:23:26 +0900

    filter study

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member", // 탐색할 패키지의 시작 위치 지정
         // @Configuration 붙은거 제외
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
